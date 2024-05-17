829510df1337d57bb5154a5a0ad9b0311eed21f6
kevinwijaya1208 <c14210124@john.petra.ac.id>
2024-05-17 10:34:54 +0700

    export database

--- pantisurya.sql
+++ pantisurya.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 14, 2024 at 09:26 AM
+-- Generation Time: May 17, 2024 at 05:34 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -41,8 +41,30 @@ CREATE TABLE `akun` (
 --
 
 INSERT INTO `akun` (`id`, `username`, `password`, `date`, `last_access`, `role`) VALUES
-(4, 'admin', '$2y$10$gRFDjBVTlkzltOIqR.E30u3ORljWTsVbGKNt.TfA6ENTsOi//ik1.', '2024-03-24', '2024-05-08', 0),
+(4, 'admin', '$2y$10$gRFDjBVTlkzltOIqR.E30u3ORljWTsVbGKNt.TfA6ENTsOi//ik1.', '2024-03-24', '2024-05-16', 0),
 (6, 'nice', '$2y$10$pcubK.aIK/YrnJKRjQlQDuJKMulGy26nbWUwOmjqKY7ATvXHzO4l6', '2024-05-08', '2024-05-14', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `images`
+--
+
+CREATE TABLE `images` (
+  `id` int(11) NOT NULL,
+  `path_picture` varchar(100) NOT NULL,
+  `input_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `images`
+--
+
+INSERT INTO `images` (`id`, `path_picture`, `input_date`) VALUES
+(1, '../asset/pp/6645f34ad6bd1.jpg', '2024-05-16'),
+(2, '../asset/pp/6645f4f95d949.jpg', '2024-05-16'),
+(3, '../asset/pp/6645f66113c5f.jpg', '2024-05-16'),
+(4, '../asset/pp/664609474f254.jpg', '2024-05-16');
 
 -- --------------------------------------------------------
 
@@ -85,38 +107,40 @@ CREATE TABLE `penduduk` (
   `keuangan_pondokkan` int(11) NOT NULL,
   `keuangan_tabungan` int(11) NOT NULL,
   `keuangan_obat` int(11) NOT NULL,
-  `keuangan_total` int(11) NOT NULL
+  `keuangan_total` int(11) NOT NULL,
+  `profile_picture` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `penduduk`
 --
 
-INSERT INTO `penduduk` (`id`, `nama`, `alamat`, `pengobatan_terakhir`, `email`, `notelp`, `keuangan_pondokkan`, `keuangan_tabungan`, `keuangan_obat`, `keuangan_total`) VALUES
-(4, 'Emily Williams', '101 Pine Street', '2024-03-14', 'emily.williams@example.com', '7894561230', 232536, 1, 21312, 0),
-(5, 'Christopher Brown', '234 Maple Street', '2024-02-25', 'christopher.brown@example.com', '9870123456', 121, 15, 5, 0),
-(7, 'David Garcia', '890 Cedar Street', '2024-02-29', 'david.garcia@example.com', '3218904567', 20, 0, 0, 0),
-(9, 'James Rodriguez', '222 Walnut Street', '2024-03-06', 'james.rodriguez@example.com', '4567890123', 0, 0, 0, 0),
-(10, 'Sarah Lee', '333 Pineapple Street', '2024-03-22', 'sarah.lee@example.com', '6543210987', 1, 0, 0, 0),
-(11, 'Daniel Lopez', '444 Grape Street', '2024-03-08', 'daniel.lopez@example.com', '3210987654', 0, 0, 0, 0),
-(12, 'Megan Harris', '555 Orange Street', '2024-03-08', 'megan.harris@example.com', '8901234567', 0, 0, 0, 0),
-(13, 'Ryan Clark', '666 Lemon Street', '2024-03-22', 'ryan.clark@example.com', '4567890123', 0, 0, 0, 0),
-(14, 'Jennifer Young', '777 Lime Street', '2024-02-29', 'jennifer.young@example.com', '1230987654', 0, 0, 0, 0),
-(15, 'Joshua Turner', '888 Strawberry Street', '2024-02-28', 'joshua.turner@example.com', '3216540987', 0, 0, 0, 0),
-(16, 'Nicole Moore', '999 Raspberry Street', '2024-02-29', 'nicole.moore@example.com', '7890123456', 0, 0, 0, 0),
-(17, 'Jason Hill', '1010 Blueberry Street', '2024-03-08', 'jason.hill@example.com', '6789012345', 0, 0, 0, 0),
-(18, 'Michelle Scott', '1111 Blackberry Street', '2024-03-12', 'michelle.scott@example.com', '7890123456', 0, 0, 0, 0),
-(19, 'Justin Green', '1212 Cranberry Street', '2024-03-12', 'justin.green@example.com', '5678901234', 0, 0, 0, 0),
-(20, 'Rachel Carter', '1313 Raspberry Street', '2024-02-25', 'rachel.carter@example.com', '8901234567', 0, 0, 0, 0),
-(21, 'Brandon King', '1414 Strawberry Street', '2024-03-12', 'brandon.king@example.com', '6789012345', 0, 0, 0, 0),
-(22, 'Melissa White', '1515 Blueberry Street', '2024-03-14', 'melissa.white@example.com', '5678901234', 0, 0, 0, 0),
-(23, 'Kevin Hall', '1616 Blackberry Street', '2024-03-08', 'kevin.hall@example.com', '7890123456', 0, 0, 0, 0),
-(24, 'Stephanie Adams', '1717 Cranberry Street', '2024-03-04', 'stephanie.adams@example.com', '6789012345', 0, 0, 0, 0),
-(25, 'Matthew Stewart', '1818 Raspberry Street', '2024-02-29', 'matthew.stewart@example.com', '8901234567', 0, 0, 0, 0),
-(26, 'Ashley Parker', '1919 Strawberry Street', '2024-03-23', 'ashley.parker@example.com', '5678901234', 0, 0, 0, 0),
-(27, 'Erica Evans', '2020 Blueberry Street', '2024-02-27', 'erica.evans@example.com', '6789012345', 0, 0, 0, 0),
-(28, 'Brian Cook', '2121 Blackberry Street', '2024-03-18', 'brian.cook@example.com', '7890123456', 0, 0, 0, 0),
-(29, 'Rebecca Murphy', '2222 Cranberry Street', '2024-03-11', 'rebecca.murphy@example.com', '6789012345', 0, 0, 0, 0);
+INSERT INTO `penduduk` (`id`, `nama`, `alamat`, `pengobatan_terakhir`, `email`, `notelp`, `keuangan_pondokkan`, `keuangan_tabungan`, `keuangan_obat`, `keuangan_total`, `profile_picture`) VALUES
+(4, 'Emily Williams', '101 Pine Street', '2024-03-14', 'emily.williams@example.com', '7894561230', 232536, 1, 21312, 0, ''),
+(5, 'Christopher Brown', '234 Maple Street', '2024-02-25', 'christopher.brown@example.com', '9870123456', 121, 15, 5, 0, ''),
+(7, 'David Garcia', '890 Cedar Street', '2024-02-29', 'david.garcia@example.com', '3218904567', 20, 0, 0, 0, ''),
+(9, 'James Rodriguez', '222 Walnut Street', '2024-03-06', 'james.rodriguez@example.com', '4567890123', 0, 0, 0, 0, ''),
+(10, 'Sarah Lee', '333 Pineapple Street', '2024-03-22', 'sarah.lee@example.com', '6543210987', 1, 0, 0, 0, ''),
+(11, 'Daniel Lopez', '444 Grape Street', '2024-03-08', 'daniel.lopez@example.com', '3210987654', 0, 0, 0, 0, ''),
+(12, 'Megan Harris', '555 Orange Street', '2024-03-08', 'megan.harris@example.com', '8901234567', 0, 0, 0, 0, ''),
+(13, 'Ryan Clark', '666 Lemon Street', '2024-03-22', 'ryan.clark@example.com', '4567890123', 0, 0, 0, 0, ''),
+(14, 'Jennifer Young', '777 Lime Street', '2024-02-29', 'jennifer.young@example.com', '1230987654', 0, 0, 0, 0, ''),
+(15, 'Joshua Turner', '888 Strawberry Street', '2024-02-28', 'joshua.turner@example.com', '3216540987', 0, 0, 0, 0, ''),
+(16, 'Nicole Moore', '999 Raspberry Street', '2024-02-29', 'nicole.moore@example.com', '7890123456', 0, 0, 0, 0, ''),
+(17, 'Jason Hill', '1010 Blueberry Street', '2024-03-08', 'jason.hill@example.com', '6789012345', 0, 0, 0, 0, ''),
+(18, 'Michelle Scott', '1111 Blackberry Street', '2024-03-12', 'michelle.scott@example.com', '7890123456', 0, 0, 0, 0, ''),
+(19, 'Justin Green', '1212 Cranberry Street', '2024-03-12', 'justin.green@example.com', '5678901234', 0, 0, 0, 0, ''),
+(20, 'Rachel Carter', '1313 Raspberry Street', '2024-02-25', 'rachel.carter@example.com', '8901234567', 0, 0, 0, 0, ''),
+(21, 'Brandon King', '1414 Strawberry Street', '2024-03-12', 'brandon.king@example.com', '6789012345', 0, 0, 0, 0, ''),
+(22, 'Melissa White', '1515 Blueberry Street', '2024-03-14', 'melissa.white@example.com', '5678901234', 0, 0, 0, 0, ''),
+(23, 'Kevin Hall', '1616 Blackberry Street', '2024-03-08', 'kevin.hall@example.com', '7890123456', 0, 0, 0, 0, ''),
+(24, 'Stephanie Adams', '1717 Cranberry Street', '2024-03-04', 'stephanie.adams@example.com', '6789012345', 0, 0, 0, 0, ''),
+(25, 'Matthew Stewart', '1818 Raspberry Street', '2024-02-29', 'matthew.stewart@example.com', '8901234567', 0, 0, 0, 0, ''),
+(26, 'Ashley Parker', '1919 Strawberry Street', '2024-03-23', 'ashley.parker@example.com', '5678901234', 0, 0, 0, 0, ''),
+(27, 'Erica Evans', '2020 Blueberry Street', '2024-02-27', 'erica.evans@example.com', '6789012345', 0, 0, 0, 0, ''),
+(28, 'Brian Cook', '2121 Blackberry Street', '2024-03-18', 'brian.cook@example.com', '7890123456', 0, 0, 0, 0, ''),
+(29, 'Rebecca Murphy', '2222 Cranberry Street', '2024-03-11', 'rebecca.murphy@example.com', '6789012345', 0, 0, 0, 0, ''),
+(43, 'nathan', 'wiyung', '2024-05-16', 'nathangg@gmail.com', '081369369', 0, 0, 0, 0, 'asset/pp/66460f957679b.jpg');
 
 -- --------------------------------------------------------
 
@@ -143,6 +167,21 @@ INSERT INTO `rekam_medis` (`penduduk_id`, `pengobatan_id`, `deskripsi`, `jenis`,
 (4, 1, 'obat1', 'pengobatan', 'panadol', 1, '2024-04-17', 1),
 (4, 2, 'obat2', 'pengobatan', 'neozep', 2, '2023-09-16', 0);
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wali`
+--
+
+CREATE TABLE `wali` (
+  `wali_id` int(11) NOT NULL,
+  `penduduk_id` int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `tempat_tinggal` varchar(100) DEFAULT NULL,
+  `agama` varchar(30) NOT NULL,
+  `no_telp` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 --
 -- Indexes for dumped tables
 --
@@ -151,6 +190,12 @@ INSERT INTO `rekam_medis` (`penduduk_id`, `pengobatan_id`, `deskripsi`, `jenis`,
 -- Indexes for table `akun`
 --
 ALTER TABLE `akun`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `images`
+--
+ALTER TABLE `images`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -167,6 +212,13 @@ ALTER TABLE `rekam_medis`
   ADD KEY `penduduk_id` (`penduduk_id`);
 
 --
+-- Indexes for table `wali`
+--
+ALTER TABLE `wali`
+  ADD PRIMARY KEY (`wali_id`),
+  ADD KEY `penduduk_id` (`penduduk_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -177,16 +229,28 @@ ALTER TABLE `akun`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
+-- AUTO_INCREMENT for table `images`
+--
+ALTER TABLE `images`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT for table `penduduk`
 --
 ALTER TABLE `penduduk`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
 
 --
 -- AUTO_INCREMENT for table `rekam_medis`
 --
 ALTER TABLE `rekam_medis`
   MODIFY `pengobatan_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `wali`
+--
+ALTER TABLE `wali`
+  MODIFY `wali_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
@@ -197,6 +261,12 @@ ALTER TABLE `rekam_medis`
 --
 ALTER TABLE `rekam_medis`
   ADD CONSTRAINT `rekam_medis_ibfk_1` FOREIGN KEY (`penduduk_id`) REFERENCES `penduduk` (`id`);
+
+--
+-- Constraints for table `wali`
+--
+ALTER TABLE `wali`
+  ADD CONSTRAINT `wali_ibfk_1` FOREIGN KEY (`penduduk_id`) REFERENCES `penduduk` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
